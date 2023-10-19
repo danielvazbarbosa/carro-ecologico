75939a1023222090b9c9ac25e0d48b2a44e0946f
Daniel <danielvaz@alu.ufc.br>
2023-10-18 23:53:00 -0300

    Uploading python files

---
+++ src/carro.py
@@ -0,0 +1,50 @@
+class Carro:
+
+    def __init__(self):
+        self.passageiros = 0
+        self.combustivel = 0
+        self.quilometragem = 0
+
+    def getPassageiros(self):
+        return self.passageiros
+
+    def getCombustivel(self):
+        return self.combustivel
+
+    def getQuilometragem(self):
+        return self.quilometragem
+
+    def embarcar(self):
+        if self.passageiros < 2:
+            self.passageiros += 1
+            return True
+        else: 
+            return False 
+
+    def desembarcar(self):
+        if self.passageiros > 0:
+            self.passageiros -= 1
+            return True
+        else:
+            return False
+
+    def dirigir(self, distancia):
+        if self.passageiros == 0 or self.combustivel == 0:
+            return False
+        else:
+            if self.combustivel - distancia < 0:
+                self.quilometragem += self.combustivel
+                self.combustivel = 0
+                return False
+            else:
+                self.quilometragem += distancia
+                self.combustivel -= distancia
+                return True
+
+    def abastecer(self, quantidade):
+        if self.combustivel == 100:
+            return False
+        else:
+            if self.combustivel + quantidade >= 100:
+                self.combustivel = 100
+                return True
